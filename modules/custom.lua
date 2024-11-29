1ed4a793ad4ffa45a0d22605af8c24f374986f04
Ironscyde <141408324+Ironscyde@users.noreply.github.com>
2024-11-29 09:48:27 +0000

    Add files via upload

--- modules/custom.lua
+++ modules/custom.lua
@@ -1,4 +1,16 @@
 --[[ startup code ]]--
+-- rgbhex(TWEAKED_PALETTE[])
+TWEAKED_PALETTES = {
+  ["BG"]     = { r = 0.27, g = 0.27, b = 0.27, colorStr = "ff464646" },
+  ["BORDER"] = { r = 0.09, g = 0.09, b = 0.09, colorStr = "ff191919" },
+  ["HEALTH"] = { r = 0.03, g = 0.88, b = 0.61, colorStr = "ff08E09C" },
+  ["HEAL"]   = { r = 0.92, g = 1,    b = 0.58, colorStr = "ffEBFF95" },
+  ["RAGE"]   = { r = 0.85, g = 0.06, b = 0.22, colorStr = "ffD90F37" },
+  ["MANA"]   = { r = 0.16, g = 0.46, b = 0.91, colorStr = "ff2A76E8" },
+  ["ENERGY"] = { r = 0.99, g = 0.81, b = 0.3,  colorStr = "ffFDCE4D" },
+  ["FOCUS"]  = { r = 1,    g = 0.41, b = 0.32, colorStr = "ffFF6952" },
+  ["PURPLE"] = { r = 0.43, g = 0.21, b = 0.84, colorStr = "ff6E36D6" },
+}
 
 pfUI:RegisterModule("custom", function ()
   --[[ module code ]]--
